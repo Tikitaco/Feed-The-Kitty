07f60eba81c7c30cdc02e7343835d93b987f79e8
Jasper Lee <j133@umd.edu>
2018-11-11 13:48:42 -0500

    Added library for Dwolla integration, added EventDetailsActivity skeleton

---
+++ app/src/main/java/com/example/stephen/fatcat/EventDetailsActivity.java
@@ -0,0 +1,16 @@
+package com.example.stephen.fatcat;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class EventDetailsActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_event_details);
+    }
+
+    //TODO Contribution options, Firebase integration, Dwolla integration
+
+}
