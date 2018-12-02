c6527f1f7aa1ba9fb4dfb5e904c347e7d509a153
Tikitaco <nytikitaco@gmail.com>
2018-12-02 16:41:44 -0500

    Made variables public

--- app/src/main/java/com/example/stephen/fatcat/com/example/stephen/fatcat/firebase/FatcatFriend.java
+++ app/src/main/java/com/example/stephen/fatcat/com/example/stephen/fatcat/firebase/FatcatFriend.java
@@ -15,9 +15,9 @@ public class FatcatFriend {
     private ArrayList<FatcatEvent> events = new ArrayList<>();
     public Map<String, Integer> invites = new HashMap<>();
     //Dwolla api information
-    String customerId;
-    Map<String, String> fundingSources = new HashMap<>();
-    Map<String, String> transfers = new HashMap<>();
+    public String customerId;
+    public Map<String, String> fundingSources = new HashMap<>();
+    public Map<String, String> transfers = new HashMap<>();
 
     // Empty constructor for firebase instances
     public FatcatFriend() {
